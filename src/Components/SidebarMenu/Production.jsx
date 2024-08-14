c32960664dc00b2438b3f9c7ae0d8473150953d1
jolly vivek <jollyvivek19@gmail.com>
2024-08-14 11:48:10 +0530

    Add Dropdown in Production Module

--- src/Components/SidebarMenu/Production.jsx
+++ src/Components/SidebarMenu/Production.jsx
@@ -1,38 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../Styles/SidebarCommon.css";
 import { FaIndustry } from "react-icons/fa";
 import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Production = () => {
+
+  const [productionItems,setProductionItems] = useState(false)
+  const [production,setProduction] = useState(false)
+  const [productionMaster,setProductionMaster] = useState(false)
+  const [foundary,setFoundary] = useState(false)
+  const [productionReports,setProductionReports] = useState(false)
+
+
+  const productionItemsDropdown = () => {
+    setProductionItems(!productionItems)
+  };
+
+  const productionDropdown = () =>{
+    setProduction(!production)
+  };
+
+  const productionMasterDropdown = () =>{
+    setProductionMaster(!productionMaster)
+  };
+
+  const foundaryDropdown = () =>{
+    setFoundary(!foundary)
+  };
+
+  const productionReportsDropdown = () =>{
+    setProductionReports(!productionReports)
+  }
+
   return (
     <div className="production">
-      <div className="production-top flex lineHeight border">
+      <div className="production-top flex lineHeight">
         <span className="icon fs-2">
           <FaIndustry />
         </span>
         <span className="title">Production</span>
-        <span className="arrow-icon">
+        <span className="arrow-icon" onClick={productionItemsDropdown}>
           <RiArrowDropDownLine />
         </span>
       </div>
-
+      
+      {productionItems ? (
       <div className="production-items-cont">
         <ul className="production-items">
-          <li className=" production-items-production flex lineHeight border ">
+          <li className=" production-items-production flex lineHeight">
             <span className="title">Production</span>
-            <span className="arrow-icon">
+            <span className="arrow-icon" onClick={productionDropdown}>
               <RiArrowDropDownLine />{" "}
             </span>
           </li>
-          <ul className="product-production-items">
-            <li className="production-master flex lineHeight border">
+          {production ? (
+          <ul className="production-items">
+            <li className="production-master flex lineHeight">
               <span  className="title">
                 Master
               </span>
-              <span className="arrow-icon">
+              <span className="arrow-icon" onClick={productionMasterDropdown}>
                 <RiArrowDropDownLine/>
               </span>
             </li>
+            {productionMaster ? (
             <ul class="production-master-items p-0">
               <li className="title"> Production Unit </li>
               <li className="title"> Operation Master </li>
@@ -41,6 +72,8 @@ const Production = () => {
               <li className="title"> Project Planning Department </li>
               <li className="title"> Machine Category</li>
             </ul>
+            ):null}
+
               <li className="title"> Work Order</li>
               <li className="title">Production Planning</li>
               <li className="title">Production Report</li>
@@ -52,11 +85,13 @@ const Production = () => {
               <li className="title">Internal Issue Stock</li>
               <li className="title">Internal Reverse Stock</li>
           </ul>
+            ):null}
 
-          <li className="foundary flex lineHeight border">
+          <li className="foundary flex lineHeight">
             <span  className="title">Foundary Production</span>
-            <span className="arrow-icon"><RiArrowDropDownLine/></span>
+            <span className="arrow-icon" onClick={foundaryDropdown}><RiArrowDropDownLine/></span>
           </li>
+          {foundary ? (
           <ul className="foundary-items">
             <li className="title">Element Master</li>
             <li className="title">Composition Master</li>
@@ -65,12 +100,14 @@ const Production = () => {
             <li className="title">Mechanical Test</li>
             <li className="title">Melting Report With Item</li>
           </ul>
+          ):null}
           <li className="title">Production Planing Register</li>
 
-          <li className="reports flex lineHeight border">
+          <li className="reports flex lineHeight">
             <span className="title" >Reports</span>
-            <span className="arrow-icon"><RiArrowDropDownLine/> </span>
+            <span className="arrow-icon" onClick={productionReportsDropdown}><RiArrowDropDownLine/> </span>
           </li>
+          {productionReports ? (
           <ul className="reports-items">
             <li className="title">Production VS Planning Report</li>
             <li className="title">Daily Production Report</li>
@@ -83,9 +120,11 @@ const Production = () => {
             <li className="title">Dispatch Report</li>
             <li className="title">CNC Production Report</li>
           </ul>
-
+          ):null}
         </ul>
       </div>
+      ):null}
+
     </div>
   );
 };
