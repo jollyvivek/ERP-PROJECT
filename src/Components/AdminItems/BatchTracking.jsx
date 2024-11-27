abd15ba48a81e25d047e84d49d9eff9d5a25f81a
jolly vivek <jollyvivek19@gmail.com>
2024-11-27 18:32:53 +0530

    Add changes in BatchTracking

--- src/Components/AdminItems/BatchTracking.jsx
+++ src/Components/AdminItems/BatchTracking.jsx
@@ -2,31 +2,57 @@ import React, { useEffect, useRef, useState } from 'react'
 
 const BatchTracking = () => {
     const [showModal, setShowModal] = useState(false);
-    // const inputRef = useRef(null);
+   const [batchData,setBatchData] = useState({
+    BatchNo:"",
+    ItemCode:"",
+    ItemName:""
+   });
+   const [selectedValue, setSelectedValue] = useState({
+    BatchNo:"",
+    ItemCode:"",
+    ItemName:""
+   });
 
-    // useEffect(() => {
-    //     if (showModal) {
-    //       const modalElement = new bootstrap.Modal(document.getElementById('exampleModal'));
-    //       modalElement.show(); 
-    //       if (inputRef.current) {
-    //         inputRef.current.focus(); 
-    //       }
-    //     }
-    //   }, [showModal]);
+   const BatchHandleChange = (event)=>{
+    const {name,value}= event.target;
+    setBatchData((data)=>({...data,[name]:value}))
+   }
+
+   const BatchSubmitHandler =()=>{
+    console.log(batchData)
+    setBatchData({ BatchNo:"", ItemCode:"", ItemName:""})
+   };
+
+   const BatchResetHandler = ()=>{
+    setBatchData({ BatchNo:"", ItemCode:"", ItemName:""})
+   }
+
+   const ModelOnChange = (event)=>{
+        const {name,value} = event.target;
+        setSelectedValue((data)=>({...data,[name]:value}))
+   }
+
+   const batchModelHandler = ()=>{
+    setShowModal(false)
+    setBatchData(selectedValue)
+   }
 
   return (
     <div className='container-fluid'>
+        <h5 className='my-2'>Master Report For Batch No</h5>
         <div className='row mt-3'>
             <div className='col-md-8 border border-secondary rounded-3 ms-2'>
                 <div className='row'>
                     <div className='col-md-6 py-1'>
                     <label htmlFor="" className="col-form-label fs-6 w-100 text-center" >Batch No :</label>
-                    <input type="text" className="form-control mt-1"  name="currentYear" autoComplete="off"
+                    <input type="text" className="form-control mt-1" readOnly  name="BatchNo" value={batchData.BatchNo} 
+                    onChange={BatchHandleChange} 
+                    autoComplete="off"
                      onClick={()=>setShowModal(true)}  />
                     </div>
                     <div className='col-md-6 py-1 d-flex align-items-center gap-2 '>
-                        <button className='btn btn-secondary mt-5'>Submit</button>
-                        <button className='btn btn-secondary mt-5'>Reset</button>
+                        <button type='button' className='btn btn-secondary mt-5' onClick={BatchSubmitHandler}>Submit</button>
+                        <button className='btn btn-secondary mt-5' onClick={BatchResetHandler}>Reset</button>
                     </div>
                 </div>
                 
@@ -39,14 +65,17 @@ const BatchTracking = () => {
                 <div className="mb-2 row">
                     <label htmlFor="" className=" col-sm-4 col-form-label fs-6  text-end" >Item Code : </label>
                 <div className="col-sm-8 d-flex align-items-center">
-                  <input type="text" className="form-control"  name="currentYear"
+                  <input type="text" className="form-control" readOnly  name="ItemCode" value={batchData.ItemCode}
+                   onChange={BatchHandleChange}
                     autoComplete="off" required  />
                 </div>
                 </div>
                 <div className="mb-2 row">
                     <label htmlFor="" className=" col-sm-4 col-form-label fs-6  text-end" >Item Name : </label>
                     <div className="col-sm-8 d-flex align-items-center">
-                        <input type="text" className="form-control"  name="currentYear" autoComplete="off" required  />
+                        <input type="text" className="form-control" readOnly   name="ItemName"  value={batchData.ItemName} 
+                         onChange={BatchHandleChange} 
+                         autoComplete="off" required  />
                     </div>
                 </div>
                 </fieldset>
@@ -69,24 +98,22 @@ const BatchTracking = () => {
                         <div className='col-md-4'>
                             <div className='row mt-3 px-1'>
                                 <label className=''>Item Code</label>
-                                {/* <input type="text"  className="form-control" placeholder="Enter text here" /> */}
                                 <select className="form-select form-select-sm" 
-                                    name='DigitalSign' aria-label="Default select example">
+                                    name='ItemCode' value={selectedValue.ItemCode} onChange={ModelOnChange} aria-label="Default select example">
                                     <option> select</option>
                                     <option value="AB101">AB101</option>
                                     <option value="AB102">AB102</option>
-                                    <option value="AB102">AB103</option>
-                                    <option value="AB102">AB104</option>
-                                    <option value="AB102">AB105</option>
+                                    <option value="AB103">AB103</option>
+                                    <option value="AB104">AB104</option>
+                                    <option value="AB105">AB105</option>
                                 </select>
                             </div>
                         </div>
                         <div className='col-md-4'>
                             <div className='row mt-3 px-1'>
                                 <label className=''>Item Name </label>
-                                {/* <input type="text"  className="form-control" placeholder="Enter text here" /> */}
                                 <select className="form-select form-select-sm" 
-                                    name='DigitalSign' aria-label="Default select example">
+                                    name='ItemName' value={selectedValue.ItemName} onChange={ModelOnChange} aria-label="Default select example">
                                     <option> select</option>
                                     <option value="Item-1">Item-1</option>
                                     <option value="Item-2">Item-2</option>
@@ -99,9 +126,8 @@ const BatchTracking = () => {
                         <div className='col-md-4'>
                             <div className='row mt-3 px-1'>
                                 <label className=''>Batch No</label>
-                                {/* <input type="text"  className="form-control" placeholder="Enter text here" /> */}
                                 <select className="form-select form-select-sm" 
-                                    name='DigitalSign' aria-label="Default select example">
+                                    name='BatchNo' value={selectedValue.BatchNo} onChange={ModelOnChange} aria-label="Default select example">
                                     <option> select</option>
                                     <option value="101">101</option>
                                     <option value="102">102</option>
@@ -117,12 +143,9 @@ const BatchTracking = () => {
             </div>
 
             <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" onClick={() => setShowModal(false)}>
-                Close
-              </button>
-              <button type="button" className="btn btn-primary">
-                Save changes
-              </button>
+            <button type="button" className="btn btn-primary" onClick={batchModelHandler}>Submit</button>
+              <button type="button" className="btn btn-secondary" onClick={() => setShowModal(false)}>Close</button>
+              
             </div>
           </div>
         </div>
