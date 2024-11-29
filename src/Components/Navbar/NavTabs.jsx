66f05e2ab3d57783d11a6aaf5252197ffb0f7f18
jolly vivek <jollyvivek19@gmail.com>
2024-11-29 12:07:25 +0530

    Add changes in NavTabs ,CompanyDetail and Index.css

--- src/Components/Navbar/NavTabs.jsx
+++ src/Components/Navbar/NavTabs.jsx
@@ -15,8 +15,8 @@ const NavTabs = () => {
 
   return (
     <div className='container-fluid'>
-      <div className='row'>
-      <div className="col-md-12 nav-btns ">
+      <div className='row '>
+      <div className="col-md-12 nav-btns d-none ">
               <button className="btn btn-sm fs-5">
                 <NavLink to="/company/*" style={NavLinkCss} className="navLink basic" >Basic Details</NavLink> 
               </button>
@@ -52,6 +52,47 @@ const NavTabs = () => {
               </button>
               {/* <button className="btn btn-sm fs-5 ">Terms & Conditions</button> */}
       </div>
+      
+      </div>
+      <div className='row mt-2'>
+        <div className='col-md-12'>
+          <ul className="nav nav-tabs px-0">
+            <li className="nav-item">
+              {/* <a className="nav-link active" aria-current="page" href="#">Active</a> */}
+              <NavLink to="/company/*" style={NavLinkCss} className="nav-link nav-link active" >Basic Details</NavLink> 
+            </li>
+            <li className="nav-item"> 
+              <NavLink to='/tax_bankdetails' style={NavLinkCss} className="navLink nav-link">Tax & Bank Details</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/terms_condtions" style={NavLinkCss} className="navLink nav-link">Terms & Condtions</NavLink> 
+            </li>
+            <li className="nav-item">
+            <NavLink to="/report_heading" style={NavLinkCss} className="navLink nav-link"> Report Heading</NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink to="/report_heading_two" style={NavLinkCss} className="navLink nav-link">Report Heading</NavLink> 
+            </li>
+            <li className="nav-item">
+              <NavLink to="/other_setting" style={NavLinkCss} className="navLink nav-link" >Other Setting</NavLink> 
+            </li>
+            <li className="nav-item">
+              <NavLink to="/crmhr_setting" style={NavLinkCss} className="navLink nav-link"> CRM/HR Setting</NavLink> 
+            </li>
+            <li className="nav-item">
+              <NavLink to="/finance_inventry_setting" style={NavLinkCss} className="navLink nav-link">Finance/Inventory Setting</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/tcs_einvoice" style={NavLinkCss} className="navLink nav-link"> TCS Setting & E-Invoice</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/costing_setting" style={NavLinkCss} className="navLink nav-link">Costing Setting</NavLink> 
+            </li>
+            <li className="nav-item">
+              <NavLink to="/production_setting" style={NavLinkCss} className="navLink nav-link">Production Setting</NavLink>
+            </li>
+            </ul>
+        </div>
       </div>
       
     </div>
