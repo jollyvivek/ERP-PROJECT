7c20f30ebf8850ce97369e9ff32cf1a098fe776a
jollyvivek <jollyvivek19@gmail.com>
2025-06-09 16:24:39 +0530

    Add backend url in StoreContext

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 
 export const StoreContext = createContext(null)
 const StoreContextProvider = (props)=>{
-const url ="http://localhost:4000";
+const url ="https://erpbackend-ybw5.onrender.com";
 const [token ,setToken] = useState("")
 const[showLogin,setShowLogin]=useState(false)
 const [userData,setUserData] = useState(null)
